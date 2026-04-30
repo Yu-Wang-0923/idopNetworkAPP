5a85dfad2494d96f5e72c9906b57654cc5a5131b
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-01 03:36:22 +0800

    Create 4_NetAnal.py

---
+++ pages/4_NetAnal.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import io
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from scipy.optimize import curve_fit
+
+st.set_page_config(page_title="网络分析", page_icon="📈")
+plt.rcParams["font.sans-serif"] = [
+    "PingFang SC",
+    "Hiragino Sans GB",
+    "Heiti SC",
+    "Microsoft YaHei",
+    "SimHei",
+    "Arial Unicode MS",
+    "DejaVu Sans",
+]
+plt.rcParams["axes.unicode_minus"] = False
+
+st.markdown("<h1 style='text-align: center;'>网络分析</h1>", unsafe_allow_html=True)
+
+
+# ========== Tabs ==========
+tab1, tab2, tab3 = st.tabs(["GLMY 同调", "中心网络", "待更新..."])
+
+
+# ========== Tab 1 ==========
+with tab1:
+    st.write("待更新...")
+
+# ========== Tab 2 ==========
+with tab2:
+    st.write("待更新...")
+
+# ========== Tab 3 ==========
+with tab3:
+    st.write("待更新...")
