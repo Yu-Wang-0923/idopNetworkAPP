a87556b8001e60251371d1875c48dde2ab182dd3
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-01 03:41:22 +0800

    Update Home.py
    expanded 侧边栏

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.set_page_config(
     page_title="idopNetwork",
     page_icon="📊",
     layout="wide",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="expanded"
 )
 
 st.markdown(
