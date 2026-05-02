eefc2f3f30e686a9dc476fd8be31b2f1bfc2054e
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-03 01:39:44 +0800

    info

--- pages/0_Curve Fitting.py
+++ pages/0_Curve Fitting.py
@@ -35,8 +35,8 @@ with tab1:
             for file in uploaded_files:
                 df_original = load_csv(file)
                 with st.expander(f"📄 Original Data: {file.name}", expanded=False):
+                    st.info(f"Rows: {df_original.shape[0]} | Columns: {df_original.shape[1]}")
                     with st.expander(f"📄 Original Data Overview: {file.name}", expanded=False):
-                        st.info(f"Rows: {df_original.shape[0]} | Columns: {df_original.shape[1]}")
                         st.dataframe(df_original, use_container_width=True)
                     with st.expander(f"📄 Descriptive Statistics: {file.name}", expanded=False):
                         st.dataframe(df_original.describe(), use_container_width=True)
