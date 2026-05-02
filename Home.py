b0afeaf75892d7fb30a563c83aadf71b4ba2f342
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-02 19:07:19 +0800

    # 调试 Curve Fitting

--- Home.py
+++ Home.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+# 添加自定义CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+ 
+# 调用函数，加载我们创建的CSS文件
+local_css("static/css/custom_style.css")
 
 st.set_page_config(
     page_title="idopNetwork",
