4ffabb8d54a5a9979a911c8a4d1deec6589a2a77
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-02 21:09:07 +0800

    uploaded_files

--- pages/0_Curve Fitting.py
+++ pages/0_Curve Fitting.py
@@ -127,14 +127,19 @@ with tab1:
                     ]
                 )
             
-            file = uploaded_files[0]
-            df = load_csv(file)
+            st.divider()
 
-            if st.session_state.get("use_index", True):
-                df = df.set_index(df.columns[0])
+            for file in uploaded_files:
+                with st.expander(f"📄 Original Data: {file.name}", expanded=False):
 
-            df_transform = data_transformation(df, scaler_type)
-            st.dataframe(df_transform.describe().round(2), use_container_width=True)
+                file = uploaded_files[0]
+                df = load_csv(file)
+
+                if st.session_state.get("use_index", True):
+                    df = df.set_index(df.columns[0])
+
+                df_transform = data_transformation(df, scaler_type)
+                st.dataframe(df_transform.describe().round(2), use_container_width=True)
         else:
             st.info("Please upload CSV file(s) to view data overview")
 
