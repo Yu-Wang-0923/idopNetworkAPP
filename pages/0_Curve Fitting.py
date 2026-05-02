fba37b150a0081163131c97aff8b1f5e741e3256
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-03 04:15:28 +0800

    show_data_expander

--- pages/0_Curve Fitting.py
+++ pages/0_Curve Fitting.py
@@ -45,20 +45,20 @@ with tab1:
                 df_original = load_csv(file)
                 st.session_state.df_original[file.name] = df_original
                 show_data_expander(f"Original Data: {file.name}", df_original)
-                with st.expander(f"Original Data: {file.name}", expanded=False):
-                    with st.expander("Data Overview", expanded=False):
-                        st.dataframe(df_original, use_container_width=True)
-                    with st.expander("Descriptive Statistics", expanded=False):
-                        st.dataframe(df_original.describe(), use_container_width=True)
-                    with st.expander("Scatter Plot", expanded=False):
-                        plot_curve_fitting(
-                                    df_scatter=df_original,
-                                    df_curve=None,
-                                    show_curve=False,
-                                    nrow=2,
-                                    ncol=3,
-                                    nsubfig=6,  # 最多画4张图
-                                    )
+                    with st.expander(f"Original Data: {file.name}", expanded=False):
+                        with st.expander("Data Overview", expanded=False):
+                            st.dataframe(df_original, use_container_width=True)
+                        with st.expander("Descriptive Statistics", expanded=False):
+                            st.dataframe(df_original.describe(), use_container_width=True)
+                        with st.expander("Scatter Plot", expanded=False):
+                            plot_curve_fitting(
+                                        df_scatter=df_original,
+                                        df_curve=None,
+                                        show_curve=False,
+                                        nrow=2,
+                                        ncol=3,
+                                        nsubfig=6,  # 最多画4张图
+                                        )
                 
                 
         else:
