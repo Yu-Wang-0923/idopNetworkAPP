fb6b75e161469162a1dd3d4354f88bb2166fdc56
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-02 23:44:01 +0800

    plot_curve_fitting

--- pages/0_Curve Fitting.py
+++ pages/0_Curve Fitting.py
@@ -289,15 +289,21 @@ def get_power_function_sample(
 
 
 
-def plot_curve_fitting(df, use_seq, n_cols, max_plots):
-    x = np.arange(1, len(df)+1) if use_seq else df.index
+def plot_curve_fitting(
+    df_quasi_dynamic, 
+    df_curve_sample
+    use_seq, 
+    n_cols, 
+    max_plots
+):
+    x = np.arange(1, len(df_quasi_dynamic)+1) if use_seq else df_quasi_dynamic.index
     cols = st.columns(n_cols)
-    selected_cols = df.columns[:max_plots]
+    selected_cols = df_quasi_dynamic.columns[:max_plots]
     for i, col in enumerate(selected_cols):
         with cols[i % n_cols]:
             fig, ax = plt.subplots(figsize=(4,3)) # , dpi=300
-            ax.scatter(x, df[col], s=150, alpha=0.7, facecolors='none', edgecolors='#4285F4', linewidth=1)
-            ax.plot(x, df[col], alpha=0.7, color='#4285F4', linewidth=1)
+            ax.scatter(x, df_quasi_dynamic[col], s=150, alpha=0.7, facecolors='none', edgecolors='#4285F4', linewidth=1)
+            ax.plot(x, df_curve_sample[col], alpha=0.7, color='#4285F4', linewidth=1)
             ax.set_title(col, fontproperties=font_prop)
             ax.set_xlabel("Sequence" if use_seq else "Index", fontproperties=font_prop)
             ax.set_ylabel(col, fontproperties=font_prop)
@@ -344,7 +350,7 @@ with tab2:
 
         curve_sample = get_power_function_sample(df_quasi_dynamic)
         st.dataframe(curve_sample, use_container_width=True)
-        plot_curve_fitting(curve_sample, use_seq = None, n_cols=2, max_plots=6)
+        plot_curve_fitting(df_quasi_dynamic, curve_sample, use_seq = None, n_cols=2, max_plots=6)
 # "Sequence"
 
 
