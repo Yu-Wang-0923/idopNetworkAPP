5bc0b105221fff0af86ac3dffaa33fc22297e11e
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-03 03:29:04 +0800

    pad

--- backend/plot_curve_fitting.py
+++ backend/plot_curve_fitting.py
@@ -22,6 +22,7 @@ def plot_curve_fitting(
     selected_cols = df_scatter.columns.tolist()
     fig, axes = plt.subplots(nrow, ncol, figsize=(8, 4), sharex=True, sharey=True, dpi=300)
     axes = axes.flatten()
+    cols = st.columns(n_cols, gap="small")
     for i, col in enumerate(selected_cols[:nsubfig]):  # 最多画 nsubfig 个
         if show_scatter:
             axes[i].scatter(df_scatter.index, df_scatter[col], alpha=0.6, s=200, facecolors='none', edgecolors='#F9B3AD', linewidth=1)
@@ -34,8 +35,29 @@ def plot_curve_fitting(
     for j in range(i + 1, len(axes)):
         axes[j].set_visible(False)
     plt.subplots_adjust(wspace=0,hspace=0)
-    plt.tight_layout()
+    plt.tight_layout(pad=0.1)
     st.pyplot(fig)
     plt.close(fig)
 
     #, facecolors='none', edgecolors='#4285F4', linewidth=1
+
+
+# # 浅色系列（适合填充、背景）
+# light_colors = [
+#     "#F9B3AD",  # 浅蜜桃粉
+#     "#EDC66A",  # 奶油黄
+#     "#9FDAF7",  # 婴儿蓝
+#     "#C9A1CA",  # 淡薰衣草紫
+#     "#D0DEE7",  # 浅雾灰蓝
+#     "#E59A9A"   # 浅豆沙红（对应第一排最右）
+# ]
+
+# # 深色系列（适合线条、描边、强调）
+# dark_colors = [
+#     "#F8A09B",  # 深蜜桃粉
+#     "#E9C060",  # 深奶油黄
+#     "#89CFF0",  # 天蓝色
+#     "#BC8FC1",  # 深薰衣草紫
+#     "#C8D7E0",  # 深雾灰蓝
+#     "#C13A3B"   # 深砖红
+# ]
