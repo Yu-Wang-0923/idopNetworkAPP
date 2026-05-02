92cc36a660d554152c1e377fa30247c27a0c9ebf
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-02 23:35:03 +0800

    plot_scatter_matrix

--- pages/0_Curve Fitting.py
+++ pages/0_Curve Fitting.py
@@ -322,3 +322,4 @@ with tab2:
 
         curve_sample = get_power_function_sample(df_quasi_dynamic)
         st.dataframe(curve_sample, use_container_width=True)
+        plot_scatter_matrix(curve_sample, use_seq = "Index", n_cols=2, max_plots=6)
