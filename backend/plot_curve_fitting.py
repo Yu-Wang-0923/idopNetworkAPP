4bd58167466a3be60cb6b7a7c6c15e9d46e394a6
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-03 03:14:59 +0800

    plot_curve_fitting

--- backend/plot_curve_fitting.py
+++ backend/plot_curve_fitting.py
@@ -24,10 +24,12 @@ def plot_curve_fitting(
     axes = axes.flatten()
     for i, col in enumerate(selected_cols[:nsubfig]):  # 最多画 nsubfig 个
         if show_scatter:
-            axes[i].scatter(df_scatter.index, df_scatter[col], alpha=0.6, s=25)
+            axes[i].scatter(df_scatter.index, df_scatter[col], alpha=0.6, s=125, facecolors='none', edgecolors='#4285F4', linewidth=1)
         axes[i].plot(df_curve.index, df_curve[col], linewidth=2.5)
         axes[i].set_title(col, fontsize=11, fontproperties=font_prop)
-        axes[i].margins(x=0.2, y=0.3)
+        axes[i].margins(x=0.1, y=0.2)
+        axes[i].xaxis.set_major_locator(plt.MaxNLocator(5))
+        axes[i].yaxis.set_major_locator(plt.MaxNLocator(5))
     # 隐藏多余子图
     for j in range(i + 1, len(axes)):
         axes[j].set_visible(False)
@@ -36,4 +38,4 @@ def plot_curve_fitting(
     st.pyplot(fig)
     plt.close(fig)
 
-    #, fontproperties=font_prop
+    #, facecolors='none', edgecolors='#4285F4', linewidth=1
