f11efbbe4679dea68c9887402140db09338b3692
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-03 04:22:08 +0800

    浅色系列（适合填充、背景）

--- backend/plot_curve_fitting.py
+++ backend/plot_curve_fitting.py
@@ -17,7 +17,7 @@ def plot_curve_fitting(
     show_curve=True,
     nrow=2,
     ncol=2,
-    nsubfig=4,  # 总子图数量
+    nsubfig=4,
 ):
     selected_cols = df_scatter.columns.tolist()
     fig, axes = plt.subplots(nrow, ncol, figsize=(6, 3), sharex=True, sharey=True, dpi=300)
@@ -42,41 +42,7 @@ def plot_curve_fitting(
     st.pyplot(fig)
     plt.close(fig)
 
-    #, facecolors='none', edgecolors='#4285F4', linewidth=1
 
-
-# # 浅色系列（适合填充、背景）
-# light_colors = [
-#     "#F9B3AD",  # 浅蜜桃粉
-#     "#EDC66A",  # 奶油黄
-#     "#9FDAF7",  # 婴儿蓝
-#     "#C9A1CA",  # 淡薰衣草紫
-#     "#D0DEE7",  # 浅雾灰蓝
-#     "#E59A9A"   # 浅豆沙红（对应第一排最右）
-# ]
-
-# # 深色系列（适合线条、描边、强调）
-# dark_colors = [
-#     "#F8A09B",  # 深蜜桃粉
-#     "#E9C060",  # 深奶油黄
-#     "#89CFF0",  # 天蓝色
-#     "#BC8FC1",  # 深薰衣草紫
-#     "#C8D7E0",  # 深雾灰蓝
-#     "#C13A3B"   # 深砖红
-# ]
-
-# dark_colors = [
-#     "#E87974",  # 加深蜜桃粉
-#     "#D8A840",  # 加深奶油黄
-#     "#5BA8D1",  # 加深天蓝色
-#     "#A06EA5",  # 加深薰衣草紫
-#     "#9FB4C2",  # 加深雾灰蓝
-#     "#9E2223"   # 加深砖红
-# ]
-
-
-import matplotlib.pyplot as plt
-import streamlit as st
 
 def plot_curve_fitting_compare(
     df_scatter_list,
@@ -157,3 +123,33 @@ def plot_curve_fitting_compare(
     plt.tight_layout()
     st.pyplot(fig)
     plt.close(fig)
+
+
+# # 浅色系列（适合填充、背景）
+# light_colors = [
+#     "#F9B3AD",  # 浅蜜桃粉
+#     "#EDC66A",  # 奶油黄
+#     "#9FDAF7",  # 婴儿蓝
+#     "#C9A1CA",  # 淡薰衣草紫
+#     "#D0DEE7",  # 浅雾灰蓝
+#     "#E59A9A"   # 浅豆沙红（对应第一排最右）
+# ]
+
+# # 深色系列（适合线条、描边、强调）
+# dark_colors = [
+#     "#F8A09B",  # 深蜜桃粉
+#     "#E9C060",  # 深奶油黄
+#     "#89CFF0",  # 天蓝色
+#     "#BC8FC1",  # 深薰衣草紫
+#     "#C8D7E0",  # 深雾灰蓝
+#     "#C13A3B"   # 深砖红
+# ]
+
+# dark_colors = [
+#     "#E87974",  # 加深蜜桃粉
+#     "#D8A840",  # 加深奶油黄
+#     "#5BA8D1",  # 加深天蓝色
+#     "#A06EA5",  # 加深薰衣草紫
+#     "#9FB4C2",  # 加深雾灰蓝
+#     "#9E2223"   # 加深砖红
+# ]
