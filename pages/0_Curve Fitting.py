ddcd9ae0cb24e711428626cb89e4d57c17b74d9d
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-02 22:01:24 +0800

    expander

--- pages/0_Curve Fitting.py
+++ pages/0_Curve Fitting.py
@@ -115,6 +115,11 @@ with tab1:
                     value=True  # 默认勾选
                 )
                 st.session_state["use_first_col_as_index"] = use_first_col_as_index
+            
+            with st.expander("⚙️ Plot Settings", expanded=False):
+                col1,col2 = st.columns(2)
+                use_seq = col1.checkbox("Use sequential X-axis", key=f"seq_{key_id}", value=True)
+                n_cols = col2.selectbox("Subplots per row", [1,2,3,4,5,6], index=2, key=f"col_{key_id}")
 
             st.divider()
 
@@ -139,10 +144,7 @@ with tab1:
 
                     key_id = id(df)
                     with st.expander("📊 Scatter Plot", expanded=False):
-                        with st.expander("⚙️ Plot Settings", expanded=False):
-                            col1,col2 = st.columns(2)
-                            use_seq = col1.checkbox("Use sequential X-axis", key=f"seq_{key_id}", value=True)
-                            n_cols = col2.selectbox("Subplots per row", [1,2,3,4,5,6], index=2, key=f"col_{key_id}")
+                        
 
                         plot_scatter_matrix(df, use_seq, n_cols)
         else:
