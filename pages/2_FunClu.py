9a5547014156d1b0cacf0f956ed1f4904e2b6944
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-03 22:40:51 +0800

    Functional Clustering

--- pages/2_FunClu.py
+++ pages/2_FunClu.py
@@ -1,24 +1,15 @@
-import io
+import sys
 import numpy as np
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-from sklearn.preprocessing import MinMaxScaler
+sys.path.append("..")
+from backend.curve_fitting import *
+from backend.plot_curve_fitting import *
 
-st.set_page_config(page_title="Functional Clustering", page_icon="📈")
-plt.rcParams["font.sans-serif"] = [
-    "PingFang SC",
-    "Hiragino Sans GB",
-    "Heiti SC",
-    "Microsoft YaHei",
-    "SimHei",
-    "Arial Unicode MS",
-    "DejaVu Sans",
-]
-plt.rcParams["axes.unicode_minus"] = False
 
-st.markdown("<h1 style='text-align: center;'>Functional Clustering</h1>", unsafe_allow_html=True)
+st.set_page_config(page_title="Functional Clustering", page_icon=None, layout="wide", initial_sidebar_state="auto")
+st.title("Functional Clustering", text_alignment="center")
+
 
 # ========== Tabs ==========
 tab1, tab2, tab3 = st.tabs(["固定簇的功能聚类", "BIC功能聚类", "待更新..."])
