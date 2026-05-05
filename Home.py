72ada16a12feb9a6d7730b730967ceccbe7e3fbc
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-05 14:14:57 +0800

    page_icon

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="idopNetwork",page_icon="📊",layout="wide",initial_sidebar_state="expanded")
+st.set_page_config(page_title="idopNetwork",page_icon="<(*￣▽￣*)/",layout="wide",initial_sidebar_state="expanded")
 
 from backend.utils import load_css
 
