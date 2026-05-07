dc9a0fc72616577767a4c24b95b385f389a83232
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-07 22:01:29 +0800

    待更新

--- pages/5_NetRecon.py
+++ pages/5_NetRecon.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # ========== 页面设置 ==========
-st.set_page_config(page_title="Network Construction", page_icon="TSA.png", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title="Network Construction111", page_icon="TSA.png", layout="wide", initial_sidebar_state="expanded")
 
 # ========== 加载 CSS ==========
 from backend.utils import load_css, setup_sidebar
@@ -9,7 +9,7 @@ load_css()
 setup_sidebar()
 
 # ========== 页面标题 ==========
-st.title("Network Construction", text_alignment="center")
+st.title("Network Construction111", text_alignment="center")
 
 
 
