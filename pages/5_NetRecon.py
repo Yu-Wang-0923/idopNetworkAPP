7b84df5179cf73ab7a265b84a53bca4855cecccf
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-07 21:59:40 +0800

    NetRecon.

---
+++ pages/5_NetRecon.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# ========== 页面设置 ==========
+st.set_page_config(page_title="Curve Fitting", page_icon="TSA.png", layout="wide", initial_sidebar_state="expanded")
+
+# ========== 加载 CSS ==========
+from backend.utils import load_css, setup_sidebar
+load_css()
+setup_sidebar()
+
+# ========== 页面标题 ==========
+st.title("Network Analysis", text_alignment="center")
+
+
+
+# ========== Tabs ==========
