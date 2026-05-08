9065c8c293de43cb85577389ba650e8d98c23809
Yu-Wang-0923 <qwertyuiopv6@163.com>
2026-05-08 23:09:28 +0800

    wu# 尺寸小一点，固定宽度 150px

--- Home.py
+++ Home.py
@@ -103,7 +103,7 @@ else:
         _, img_col, _ = st.columns([1.5, 4, 1.5])
         with img_col:
             st.markdown("<div style='margin-top: 1rem;'></div>", unsafe_allow_html=True)
-            st.image("wu.jpg", use_container_width=True) 
+            st.image("wu.jpg", width=150)  # 尺寸小一点，固定宽度 150px
             st.markdown("<p style='text-align:center; font-weight:bold; font-size:1.15rem; color:#0f172a; margin-top:10px; margin-bottom:2px;'>邬荣领 教授</p>", unsafe_allow_html=True)
             st.markdown("<p style='text-align:center; color:#475569; font-size:0.95rem; margin-top:0;'>BIMSA 副院长 / 首席科学家</p>", unsafe_allow_html=True)
             st.markdown("""
@@ -113,6 +113,7 @@ else:
                 </a>
             </div>
             """, unsafe_allow_html=True)
+       
 
     st.divider()
 
